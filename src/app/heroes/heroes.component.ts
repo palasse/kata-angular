742e2c4faaee7168f57eaacc046e4ffd4ffe09e2
unknown <nohan.palasse@epitech.eu>
2022-03-10 14:22:08 +0100

    kata session 3 - version moche

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  hero:Hero = { id: 1, name:'Windstorm'};
+  // hero:Hero = { id: 1, name:'Windstorm'};
+  heroes: Array<Hero> = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
 
+  }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
