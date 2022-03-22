e4ec83034a79ffeb7221e4961eed3f90a46eaabd
unknown <nohan.palasse@epitech.eu>
2022-03-22 13:48:34 +0100

    feat: Add heroes services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'my-heroes',
@@ -10,17 +10,22 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   // hero:Hero = { id: 1, name:'Windstorm'};
-  heroes: Array<Hero> = HEROES;
+  heroes: Array<Hero> = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+      this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
   }
 
 }
